e32996b13ef0f2b4cce53b2914a5c396e3042fb4
bs@nostromo <brandon.sltaer94@gmail.com>
2019-04-23 15:32:38 -0800

    added typewriter

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 //Game.js
-var input = false;
-    disp = false;
+var input = false,
+    disp = false,
     lg = false;
 
 $(document).ready(function () {
@@ -8,17 +8,47 @@ $(document).ready(function () {
     disp = $("#display");
     lg = $("#log");
     input.focusin();
+    $(this).click(function(){input.focusin()})
     input.keypress(function (e) {
-        if (e.which == 13) {
-            if(input.val() == "start") {lvlone();};
-
-            lg.prepend("<li>"+input.val()+"</li>");
-            input.val("");
-        }
+        lvlFilter(e)
     });
 })
 
+var display = new Object({
+    loopObj : {i:0,speed:50,disptxt:""},
+    RunLevel : function(text){
+        display.loopObj.disptxt = text
+        display.TypeWriter()
+    },
+    TypeWriter : function(){
+        if (display.loopObj.i < display.loopObj.disptxt.length) {
+            disp[0].value += display.loopObj.disptxt.charAt(display.loopObj.i);
+            display.loopObj.i++;
+            setTimeout(function(){display.TypeWriter()}, display.loopObj.speed);
+        } else {
+            display.loopObj.i == 0
+        }
+    }
+})
+
+function consoleLog(){
+    lg.prepend("<li>"+input.val()+"</li>"); 
+    input.val("");
+}
+
+
+
+function lvlFilter(event){
+    if (event.which == 13) {
+        switch (input.val()){
+            case "start": lvlone(); break;
+            default : consoleLog();
+        }
+    }
+}
 
 function lvlone(){
-    disp.val("Hello world")
+    // display.loopObj.disptxt = "Hello"
+    display.RunLevel("Hello world! Welcome to spriteconverter.js").done()
+    // display.RunLevel("Please wait for instructions...")
 }
